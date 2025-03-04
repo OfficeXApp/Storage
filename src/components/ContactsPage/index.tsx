f89047f6b5ff5cc30c693973dce5227c7e4a677a
mevdragon <admin@officex.app>
2025-03-05 06:11:02 +0700

    add rich sidebar to ui

---
+++ src/components/ContactsPage/index.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { Button, Input, Layout, Typography, message } from "antd";
+import type {
+  Disk,
+  IRequestCreateDisk,
+  IRequestListDisks,
+  IResponseCreateDisk,
+  IResponseListDisks,
+} from "@officexapp/types";
+import { DiskTypeEnum } from "@officexapp/types";
+import { useDispatch, useSelector } from "react-redux";
+import { AppState } from "../../store/store";
+import { createDisk, fetchDisks } from "../../store/disks/disks.actions";
+const { Content, Footer } = Layout;
+const { Title, Paragraph, Text } = Typography;
+
+const ContactsPage = () => {
+  return (
+    <Layout style={{ minHeight: "100vh", backgroundColor: "white" }}>
+      <Content style={{ padding: "0 16px", maxWidth: "800px", gap: 16 }}>
+        <Title level={1} style={{ fontWeight: "bold" }}>
+          Contacts
+        </Title>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>OfficeX ©2024</Footer>
+    </Layout>
+  );
+};
+
+export default ContactsPage;
